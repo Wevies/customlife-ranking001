8032212c7ddd581c035c413da89fc95aed83bf1e
柿本大志 <lilybell0412@gmail.com>
2021-03-18 21:09:06 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -7,11 +7,85 @@ function switchClass(i) {
     lis.eq(i).removeClass('home_header_out');
     lis.eq(i = ++i % lis.length).addClass('home_header_on');
     lis.eq(i = ++i % lis.length).addClass('home_header_out');
-    setTimeout(function() {
+    setTimeout(function () {
         switchClass(i);
     }, 3500);
 }
 
-$(window).load(function() {
+$(window).load(function () {
     switchClass(-1);
 });
+
+
+/**
+ * 
+ * scroll-in-js
+ * 
+ */
+
+
+
+$(function () {
+    $(window).scroll(function () {
+        $(".js-scroll").each(function () {
+            let imgPos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + 10) {
+                $(this).addClass("js-scroll-in");
+                $(this).removeClass("js-scroll");
+            }
+        });
+    });
+
+    $(window).scroll(function () {
+        $(".js-scroll-blur").each(function () {
+            let imgPos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + 10) {
+                $(this).addClass("js-scroll-blur-in");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".effect").each(function () {
+            let imgPos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + windowHeight / 5 + 50) {
+                $(this).addClass("effect-in");
+            }
+        });
+    });
+});
+// $(function () {
+//     $(".js-scroll").each(function () {
+//         let imgPos = $(this).offset().top;
+//         let scroll = $(window).scrollTop();
+//         let windowHeight = $(window).height();
+//         if (scroll > imgPos - windowHeight + 50) {
+//             $(this).addClass("js-scroll-in");
+//         }
+//     });
+// });
+// $(function () {
+//     $(".js-scroll-blur").each(function () {
+//         let imgPos = $(this).offset().top;
+//         let scroll = $(window).scrollTop();
+//         let windowHeight = $(window).height();
+//         if (scroll > imgPos - windowHeight + 50) {
+//             $(this).addClass("js-scroll-blur-in");
+//         }
+//     });
+// });
+// $(function () {
+//     $(".effect-in").each(function () {
+//         let imgPos = $(this).offset().top;
+//         let scroll = $(window).scrollTop();
+//         let windowHeight = $(window).height();
+//         if (scroll > imgPos - windowHeight + windowHeight / 5 + 50) {
+//             $(this).addClass("effect-in");
+//         }
+//     });
+// });
